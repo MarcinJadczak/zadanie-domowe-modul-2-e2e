ec1f076193ed09cde08fbff60aa87c8822a17b06
MarcinJadczak <jadczak.martin@gmail.com>
2024-01-25 17:54:04 +0100

    zmiany

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.edu.goit.global/account/login",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,20 +1,31 @@
+import Login from "../pages/Login";
+import Logout from "../pages/HomePage";
+
+const login = new Login();
+const logout = new Logout();
+
 describe("User can login and logout to GoIT page", () => {
-  it("login and logout", () => {
-    cy.visit("https://www.edu.goit.global/account/login");
-    cy.loginUser("user888@gmail.com", "1234567890");
-    cy.get('[data-element-type="burger-menu"]').click();
-    // cy.get(":nth-child(11) > .next-bve2vl").click();
-    cy.get(".next-bve2vl").contains("Log out").click();
+  beforeEach("User can visit to goit page", () => {
+    cy.visit("/");
   });
 
-  it("Other way to login and logout on another user", () => {
-    cy.visit("https://www.edu.goit.global/account/login");
-    cy.fillEmailInput("testowyqa@qa.team");
-    cy.fillPasswordInput("QA!automation-1");
-    // cy.get("#user_email").type("testowyqa@qa.team");
-    // cy.get("[name=password]").type("QA!automation-1");
-    cy.get('button[type="submit"]').click();
-    cy.get('[data-element-type="burger-menu"]').click();
-    cy.get(".next-bve2vl").contains("Log out").click();
+  it("Login and logout user 1", () => {
+    cy.fixture("user1.json").then((user) => {
+      const useremail = user.email;
+      const password = user.password;
+
+      login.loginUser(useremail, password);
+      logout;
+    });
+  });
+
+  it("Login and logout user 2", () => {
+    cy.fixture("user2.json").then((user) => {
+      const useremail = user.email;
+      const password = user.password;
+
+      cy.loginUser(useremail, password);
+      logout;
+    });
   });
 });
